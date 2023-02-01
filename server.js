359f738e16303d0ae5e3d1c81ddfa76a2767d22a
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2023-02-01 18:00:41 +0700

    add all hospital routes

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// load dotenv vars
 dotenv.config({ path: "./config/config.env" });
 
-const app = express();
+// route file
+const hospitals = require("./routes/hospitals");
 
-app.get("/", (req, res) => {
-  //   res.send("<h1>Hello from express</h1>");
-  //   res.send({ name: "Brad" });
-  //   res.json({ name: "Brad" });
-  //   res.sendStatus(400);
-  //   res.status(400).json({ success: false });
-  res.status(200).json({ success: true, data: { id: 1 } });
-});
+const app = express();
+app.use("/api/v1/hospitals", hospitals);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
