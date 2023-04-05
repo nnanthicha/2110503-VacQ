aaa10a1f71934e5b41dd30a1053cfff07e891ad2
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2023-04-05 18:25:53 +0700

    security and swagger

--- vacqbackend/controllers/auth.js
+++ vacqbackend/controllers/auth.js
@@ -59,16 +59,29 @@ exports.login = async (req, res, next) => {
       .json({ success: false, msg: "Please provide email and password" });
   }
   // Check for user
-  const user = await User.findOne({ email }).select("+password");
-  if (!user) {
-    return res.status(400).json({ success: false, msg: "Invalid credentials" });
+  try {
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Invalid credentials" });
+    }
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Invalid credentials" });
+    }
+    sendTokenResponse(user, 200, res);
+  } catch (error) {
+    return res
+      .status(400)
+      .json({
+        success: false,
+        msg: "Cannot convert email or password to string",
+      });
   }
-  // Check if password matches
-  const isMatch = await user.matchPassword(password);
-  if (!isMatch) {
-    return res.status(400).json({ success: false, msg: "Invalid credentials" });
-  }
-  sendTokenResponse(user, 200, res);
 };
 
 // @desc    Get current logged in user
@@ -77,4 +90,16 @@ exports.login = async (req, res, next) => {
 exports.getMe = async (req, res, next) => {
   const user = await User.findById(req.user.id);
   res.status(200).json({ success: true, data: user });
+};
+
+// @desc    Log user out / clear cookie
+// @route   POST /api/v1/auth/logout
+// @access  Private
+exports.logout = async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({ success: true, data: {} });
 };
--- vacqbackend/server.js
+++ vacqbackend/server.js
@@ -2,7 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
-const cors = require('cors');
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 // load dotenv vars
 dotenv.config({ path: "./config/config.env" });
@@ -20,12 +27,51 @@ app.use(cors());
 
 // Body parser
 app.use(express.json());
-app.use("/api/v1/hospitals", hospitals);
-app.use("/api/v1/auth", auth);
-app.use("/api/v1/appointments", appointments);
 
 // Cookie parser
 app.use(cookieParser());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+app.use(limiter);
+
+// Prevent http param pollutions
+app.use(hpp());
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express VacQ API",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000/api/v1",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/appointments", appointments);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
